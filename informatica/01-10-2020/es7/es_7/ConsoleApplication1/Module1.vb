85d5621cdb05ad96f825d637fcfdcb22ac05d0e5
Nicholas-scuola <56036434+nicholas-progetti-scuola@users.noreply.github.com>
2020-10-01 17:46:56 +0200

    Add files via upload

---
+++ informatica/01-10-2020/es7/es_7/ConsoleApplication1/Module1.vb
@@ -0,0 +1,39 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim prezzo As Double
+
+
+        prezzo = InputBox("inserisci prezzo", "Cassa degli sconti", "0", -1, -1)
+
+
+
+
+        
+
+
+
+
+        If (prezzo < 50) Then
+
+            prezzo = prezzo - (prezzo / 100) * 10
+
+            MsgBox("il prezzo scontato è " + CType(prezzo, String), MsgBoxStyle.Information, "sconto")
+
+
+
+        ElseIf (prezzo > 50 And prezzo <= 100) Then
+            prezzo = prezzo - (prezzo * 20 / 100)
+            MsgBox("il prezzo scontato è " + CType(prezzo, String), MsgBoxStyle.Information, "sconto")
+        Else
+            prezzo = prezzo - (prezzo * 30 / 100)
+            MsgBox("il prezzo scontato è " + CType(prezzo, String), MsgBoxStyle.Information, "sconto")
+
+
+
+        End If
+
+
+    End Sub
+
+End Module
---
+++ informatica/01-10-2020/es_12/ConsoleApplication1/ConsoleApplication1/Module1.vb
@@ -0,0 +1,24 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim età As Integer
+        età = InputBox("inserisci l'eta per calcolare lo sconto :) ", "Calcolatore di sconto V3")
+
+
+        If (età < 5) Then
+            MsgBox("congratulazioni il tuo ingresso è gratis", MsgBoxStyle.Information, "prezzo")
+
+
+        ElseIf (età <= 10) Then
+            MsgBox("il tuo ingresso è di 1 misero euro", MsgBoxStyle.Information, "prezzo")
+        ElseIf (età <= 17) Then
+            MsgBox("il tuo ingresso è di 1.50 euro", MsgBoxStyle.Information, "prezzo")
+        ElseIf (età <= 26) Then
+            MsgBox("il tuo ingresso è di 2 euro come il prezzo di 2 kaffeeeeee", MsgBoxStyle.Information, "prezzo")
+        ElseIf (età > 26) Then
+            MsgBox("il tuo ingresso è di 3 euro tondi tondi", MsgBoxStyle.Information, "prezzo")
+        End If
+        Console.ReadKey()
+    End Sub
+
+End Module
