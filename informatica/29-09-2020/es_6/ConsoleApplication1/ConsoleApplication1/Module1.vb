dae068bc9a1b9d58d9dad2b08cc30f2f61757d01
Nicholas-scuola <56036434+nicholas-progetti-scuola@users.noreply.github.com>
2020-10-01 17:59:49 +0200

    Add files via upload

---
+++ informatica/29-09-2020/es_6/ConsoleApplication1/ConsoleApplication1/Module1.vb
@@ -0,0 +1,35 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim a, b, c, x1, x2, delta As Double
+
+
+        Console.WriteLine("Inserire il coefficiente dell'incognita a")
+        a = Console.ReadLine()
+        Console.WriteLine("Inserire il coefficiente dell'incognita b")
+        b = Console.ReadLine()
+        Console.WriteLine("Inserire il coefficiente di c")
+        c = Console.ReadLine()
+        delta = b * b - 4 * a * c
+        If (delta < 0.0) Then
+            MsgBox("il delta negativo rende equazione impossibile ", MsgBoxStyle.Critical)
+        Else
+            x1 = (-b + Math.Sqrt(b * b - 4 * a * c)) / 2 * a
+            x2 = (-b - Math.Sqrt(b * b - 4 * a * c)) / 2 * a
+            Console.WriteLine("Il delta vale " + CType(delta, String) + " x1 = " + CType(x1, String) + " x2 = " + CType(x2, String))
+
+
+        End If
+
+
+
+
+
+        Console.ReadKey()
+
+
+
+
+    End Sub
+
+End Module
---
+++ informatica/29-09-2020/numero_maggiore/numero_maggiore/Module1.vb
@@ -0,0 +1,24 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim num, num1, appoggio As Int32
+        num = InputBox("inserisci il primo numero ")
+        num1 = InputBox("inserisci il secondo numero ")
+        If (num1 > num) Then
+            appoggio = num
+            num = num1
+            num1 = appoggio
+        End If
+        appoggio = num - num1
+        MsgBox("il numero maggiore è" + CType(num, String
+               ) + " quelllo minore è " + CType(num1, String
+               ) + " la differenza è " + CType(appoggio, String
+               ))
+
+
+
+
+
+    End Sub
+
+End Module
