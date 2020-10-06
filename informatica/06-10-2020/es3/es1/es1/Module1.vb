744ff31e6f263ceea4ce00c4c6c9eae6a2ee286e
Nicholas-scuola <56036434+nicholas-progetti-scuola@users.noreply.github.com>
2020-10-06 14:06:53 +0200

    Add files via upload

---
+++ informatica/06-10-2020/es3/es1/es1/Module1.vb
@@ -0,0 +1,37 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim giorno1, mese1, anno1, giorno2, mese2, anno2 As Integer
+        giorno1 = InputBox("inserisci il giorno della prima data da analizzare", "Calcolatore date")
+        mese1 = InputBox("inserisci il mese della prima data da analizzare", "Calcolatore date")
+        anno1 = InputBox("inserisci l'anno della prima data da analizzare", "Calcolatore date")
+
+        giorno2 = InputBox("inserisci il giorno della seconda data da analizzare", "Calcolatore date")
+        mese2 = InputBox("inserisci il mese della seconda data da analizzare", "Calcolatore date")
+        anno2 = InputBox("inserisci il giorno della seconda data da analizzare", "Calcolatore date")
+
+        If (anno1 > anno2) Then
+            MsgBox("La data più recente è " & giorno1 & "/" & mese1 & "/" & anno1,MsgBoxStyle.Information)
+        ElseIf (anno1 < anno2) Then
+            MsgBox("La data più recente è " & giorno2 & "/" & mese2 & "/" & anno2, MsgBoxStyle.Information)
+
+        Else
+            If (mese1 > mese2) Then
+                MsgBox("La data più recente è " & giorno1 & "/" & mese1 & "/" & anno1, MsgBoxStyle.Information)
+            ElseIf (mese1 < mese2) Then
+                MsgBox("La data più recente è " & giorno2 & "/" & mese2 & "/" & anno2, MsgBoxStyle.Information)
+
+            Else
+                If (giorno1 > giorno2) Then
+                    MsgBox("La data più recente è " & giorno1 & "/" & mese1 & "/" & anno1, MsgBoxStyle.Information)
+                ElseIf (giorno1 < giorno2) Then
+                    MsgBox("La data più recente è " & giorno2 & "/" & mese2 & "/" & anno2, MsgBoxStyle.Information)
+                Else
+                    MsgBox("Le due date sono uguali", MsgBoxStyle.Information)
+                End If
+            End If
+        End If
+
+    End Sub
+
+End Module
---
+++ informatica/06-10-2020/es3/es2/es2/Module1.vb
@@ -0,0 +1,27 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim giorno1, mese1, giorno2, mese2, giorno3, mese3 As Integer
+        giorno1 = InputBox("Inserire il giorno della prima data", "Calcolatore data")
+        mese1 = InputBox("Inserire il mese della prima data", "Calcolatore data")
+
+        giorno2 = InputBox("Inserire il giorno della seconda data", "Calcolatore data")
+        mese2 = InputBox("Inserire il giorno della seconda data", "Calcolatore data")
+
+        giorno3 = InputBox("Inserire il giorno della terza data", "Calcolatore data")
+        mese3 = InputBox("Inserire il mese della terza data", "Calcolatore data")
+
+        If (mese1 <= mese3 And mese2 >= mese3 Or mese1 >= mese3 And mese2 <= mese3) Then
+            MsgBox("La data " + CType(giorno3,String)+"/" + Ctype(mese3,String),MsgBoxStyle.MsgBoxHelp)
+
+        ElseIf (giorno1 <= giorno3 And giorno2 >= giorno3 Or giorno1 >= giorno3 And giorno2 <= giorno3) Then
+            MsgBox("La data " + CType(giorno3, String) + "/" + CType(mese3, String), MsgBoxStyle.MsgBoxHelp)
+
+        Else
+            MsgBox("le date sono uguali ")
+
+        End If
+
+    End Sub
+
+End Module
