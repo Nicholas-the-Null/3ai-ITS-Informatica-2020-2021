9f1fcbdf563dd3973fceda6c570968633b164ec2
Nicholas-scuola <56036434+nicholas-progetti-scuola@users.noreply.github.com>
2020-10-06 13:29:36 +0200

    Add files via upload

---
+++ informatica/06-10-2020/es3/es3/Module1.vb
@@ -0,0 +1,49 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim cilindrata, giorni, prezzo As Integer
+        Dim navigatore As String = ""
+
+
+        MsgBox("Scegliere cilindrata 1-piccola, 2-media, 3-grande uso i numeri", MsgBoxStyle.Information, "Cilindrata")
+
+        cilindrata = InputBox("inserisci cilindrata", "Calcolatore Cilindrata ")
+
+        Select Case cilindrata
+            Case 1
+                prezzo = 30
+            Case 2
+                prezzo = 50
+            Case 3
+                prezzo = 90
+                navigatore = LCase(InputBox("vuoi il navigatore y/n"))
+
+            Case Else
+                MsgBox("Cilindrata non valida pertanto di defaul verrà inserito la grande", MsgBoxStyle.Critical)
+                prezzo = 90
+
+
+        End Select
+
+        giorni = InputBox("inserisci i giorni ")
+
+        If giorni > 7 Then
+            prezzo = prezzo - (prezzo / 100) * 10
+
+
+
+
+        End If
+
+        prezzo = prezzo * giorni
+
+        If navigatore = "y" Then
+            prezzo += 35
+
+        End If
+
+        MsgBox("Il prezzo è " + CType(prezzo, String),MsgBoxStyle.Information)
+
+    End Sub
+
+End Module
---
+++ informatica/06-10-2020/es3/es3/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ConsoleApplication1")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("ConsoleApplication1")> 
+<Assembly: AssemblyCopyright("Copyright ©  2020")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("94090898-138b-4fe3-9db3-b84ae3cb1e28")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
