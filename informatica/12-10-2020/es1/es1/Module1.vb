7f8500d61590c3587fa6042a6d0e2565344981b5
Nicholas-scuola <56036434+nicholas-progetti-scuola@users.noreply.github.com>
2020-10-12 15:55:44 +0200

    Add files via upload

---
+++ informatica/12-10-2020/es1/es1/Module1.vb
@@ -0,0 +1,17 @@
+﻿'Dato in input un numero calcolare la somma di tutti i numeri =< dispari di esso
+Module Module1
+
+    Sub Main()
+        Dim somma, num As Integer
+        num = InputBox("inserisci il numero che vuoi clacolare", "calcolatore")
+        For i = 0 To num
+            If (i Mod 2 <> 0) Then
+                somma += i
+            End If
+        Next i
+        MsgBox("la somma è " & somma)
+
+
+    End Sub
+
+End Module
