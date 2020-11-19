a58eccbb004016fbc660cc1eaf33dfab92452379
Nicholas-scuola <56036434+nicholas-progetti-scuola@users.noreply.github.com>
2020-11-19 16:17:28 +0100

    Add files via upload

---
+++ informatica/17-11-2020/es6/es6/Module1.vb
@@ -0,0 +1,33 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim numero_elementi, n As Integer
+        Dim a As Boolean = False
+        Do While True
+            numero_elementi = InputBox("Inserire quanti elementi vuoi nel vettore.", "RICHIESTA")
+            If numero_elementi > 0 Then
+                Exit Do
+            Else
+                MsgBox("errore valore inserito non valido", MsgBoxStyle.Exclamation, "errore")
+            End If
+        Loop
+
+        Dim vet(numero_elementi) As Integer
+        For x = 0 To UBound(vet) - 1
+            vet(x) = InputBox("Inserire un numero.", "RICHIESTA")
+        Next
+        n = InputBox("Inserire il numero da cercare nel vettore.", "RICHIESTA")
+        For x = 0 To UBound(vet) - 1
+            If vet(x) = n Then
+                MsgBox("Il numero " & n & " si trova alla posizione " & x, MsgBoxStyle.Information, "INFORMAZIONE")
+                a = True
+                Exit For
+            End If
+        Next
+        If a = False Then
+            MsgBox("Indice non valido", MsgBoxStyle.Critical, "ERRORE")
+        End If
+
+    End Sub
+
+End Module
---
+++ informatica/17-11-2020/es7/es7/Module1.vb
@@ -0,0 +1,32 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim nn As Integer
+        Dim stringa_a, sb, sc As String
+        Do While True
+            nn = InputBox("Inserire quanti elementi vuoi nel vettore.", "RICHIESTA")
+            If nn > 0 Then
+                Exit Do
+            Else
+                MsgBox("errore valore inserito non valido", MsgBoxStyle.Exclamation, "errore")
+            End If
+        Loop
+        Dim a(nn), b(nn), c(nn) As Integer
+        stringa_a = ""
+        sb = ""
+        sc = ""
+        For x = 0 To nn - 1
+            a(x) = InputBox("Inserire un elemento del vettore a.", "RICHIESTA")
+            b(x) = InputBox("Inserire un elemento del vettore b.", "RICHIESTA")
+            c(x) = Math.Round((a(x) + b(x)) / 2, 2)
+            stringa_a += a(x).ToString + " "
+            sb += b(x).ToString + " "
+            sc += c(x).ToString + " "
+        Next
+        MsgBox("Il vettore a è : " & stringa_a & ".", MsgBoxStyle.Information, "INFORMAZIONE")
+        MsgBox("Il vettore b è : " & sb & ".", MsgBoxStyle.Information, "INFORMAZIONE")
+        MsgBox("Il vettore c è : " & sc & ".", MsgBoxStyle.Information, "INFORMAZIONE")
+
+    End Sub
+
+End Module
