03fecd5690947a9b1ee915bc1b311c0bd968e8aa
Nicholas-scuola <56036434+nicholas-progetti-scuola@users.noreply.github.com>
2020-11-22 15:28:43 +0100

    Add files via upload

---
+++ informatica/17-11-2020/es8/es8/Module1.vb
@@ -0,0 +1,35 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim n, vetp(0), vetn(0), counter_pos, counter_neg As Integer
+        Dim out_pos, out_neg As String
+        out_pos = ""
+        out_neg = ""
+        counter_pos = 0
+        counter_neg = 0
+        For x = 0 To 59
+            Try
+                n = Int(InputBox("Inserisci il numero da inserire", "Inserire"))
+                If n > 0 Then
+                    out_pos += n.ToString + ";"
+                    vetp(x) = n
+                    counter_pos += 1
+                    ReDim Preserve vetp(counter_pos)
+                ElseIf n < 0 Then
+                    out_neg += n.ToString + ";"
+                    vetn(x) = n
+                    counter_neg += 1
+                    ReDim Preserve vetn(counter_neg)
+                End If
+            Catch ex As Exception
+                MsgBox("errore di " & ex.Message, MsgBoxStyle.Critical, "Errore")
+                x -= 1
+
+            End Try
+        Next
+        MsgBox("array positivi " & out_pos, MsgBoxStyle.Information)
+        MsgBox("array negativi " & out_neg, MsgBoxStyle.Information)
+
+    End Sub
+
+End Module
---
+++ informatica/17-11-2020/es9/es9/Module1.vb
@@ -0,0 +1,25 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim vet(21), n As Integer
+        Dim svet As String = ""
+        For x = 0 To UBound(vet) - 1
+            Try
+                n = InputBox("Inserire un numero positivo.", "RICHIESTA")
+                If n < 0 Then
+                    MsgBox("Numero inserito non valido.", MsgBoxStyle.Critical, "ERRORE")
+                    x -= 1
+                Else
+                    vet(x) = n
+                    svet += n.ToString + " "
+                End If
+            Catch ex As Exception
+                MsgBox("errore " & ex.Message, MsgBoxStyle.Critical, "Errore")
+
+            End Try
+        Next
+        MsgBox("Il contenuto del vettore è : " + svet, MsgBoxStyle.Information, "INFORMAZIONE")
+
+    End Sub
+
+End Module
