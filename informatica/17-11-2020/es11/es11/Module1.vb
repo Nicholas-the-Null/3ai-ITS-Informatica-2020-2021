6b1debed8a9dbd8a475975fa409d034f3740b93b
Nicholas-scuola <56036434+nicholas-progetti-scuola@users.noreply.github.com>
2020-11-24 21:11:03 +0100

    Add files via upload

---
+++ informatica/17-11-2020/es11/es11/Module1.vb
@@ -0,0 +1,98 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim comando, contatore, posizione As Integer
+        contatore = 0
+        Dim vet_contatti(contatore), vet_num(contatore), servitore As String
+        Dim verificatore As Boolean
+        Do While True
+            Console.WriteLine()
+            Console.WriteLine("---------------------------------------")
+            For g = 0 To UBound(vet_contatti) - 1
+                Console.Write(vet_contatti(g) & " ")
+
+            Next
+
+            Console.WriteLine()
+            For g = 0 To UBound(vet_num) - 1
+                Console.Write(vet_num(g) & " ")
+
+            Next
+
+
+            Try
+                comando = Int(InputBox("inserisci il comando 1-inserisci contatto,2-elimina contatto,3cerca contatto da nome,0-esci"))
+            Catch ex As Exception
+                MsgBox("Errore di " & ex.Message, MsgBoxStyle.Critical)
+                comando = 5
+
+            End Try
+
+
+            Select Case comando
+                Case 0
+                    MsgBox("Sto uscendo", MsgBoxStyle.Information)
+                    Exit Do
+
+                Case 1
+                    vet_contatti(contatore) = InputBox("inserisci il nome", "Agenda 3.0")
+                    vet_num(contatore) = InputBox("inserisci il numero ", "Agenda 3.0")
+                    contatore += 1
+                    ReDim Preserve vet_contatti(contatore), vet_num(contatore)
+
+                Case 2
+                    verificatore = False
+                    servitore = InputBox("Inserire il nome del contatto da cancellare", "RICHIESTA")
+                    For x = 0 To UBound(vet_contatti) - 1
+                        If servitore = vet_contatti(x) Then
+                            verificatore = True
+                            For i = x To UBound(vet_contatti) - 1
+                                vet_contatti(i) = vet_contatti(i + 1)
+                            Next i
+                            For i = x To UBound(vet_contatti) - 1
+                                vet_num(i) = vet_num(i + 1)
+                            Next i
+                            contatore -= 1
+                            ReDim Preserve vet_contatti(contatore), vet_num(contatore)
+                        End If
+                    Next x
+                    If verificatore = False Then
+                        MsgBox("Nome non trovato",MsgBoxStyle.Critical)
+                    Else
+                        MsgBox("occorenza eliminata ", MsgBoxStyle.Information)
+                    End If
+
+
+                Case 3
+                    verificatore = False
+                    posizione = 0
+                    servitore = InputBox("dammi il nome")
+                    For i = 0 To UBound(vet_contatti) - 1
+                        If servitore = vet_contatti(i) Then
+                            posizione = i
+                            verificatore = True
+                        End If
+                    Next
+
+                    If verificatore = True Then
+                        For x = 0 To UBound(vet_num)
+                            If x = posizione Then
+                                MsgBox("il numero di telefono di " & servitore & " è " & vet_num(x), MsgBoxStyle.Information)
+                                Exit For
+                            End If
+                        Next
+                    Else
+                        MsgBox("ERRORE!!! utente non trovato", MsgBoxStyle.Critical)
+                    End If
+
+
+                Case Else
+                    MsgBox("ERRORE!!! Comando inserito non valido.", MsgBoxStyle.Critical, "ERRORE")
+            End Select
+
+
+        Loop
+
+    End Sub
+
+End Module
---
+++ informatica/17-11-2020/es11/es11/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Le informazioni generali relative a un assembly sono controllate dal seguente 
+' set di attributi. Modificare i valori di questi attributi per modificare le informazioni
+' associate a un assembly.
+
+' Controllare i valori degli attributi degli assembly
+
+<Assembly: AssemblyTitle("es11")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("es11")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Se il progetto viene esposto a COM, il GUID seguente verrà usato come ID del typelib
+<Assembly: Guid("b63fbf5e-acf6-4d5a-b2bd-92e0c77f97f3")>
+
+' Le informazioni sulla versione di un assembly sono costituite dai seguenti quattro valori:
+'
+'      Versione principale
+'      Versione secondaria
+'      Numero di build
+'      Revisione
+'
+' È possibile specificare tutti i valori oppure impostare valori predefiniti per i numeri relativi alla revisione e alla build
+' usando l'asterisco '*' come illustrato di seguito:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
