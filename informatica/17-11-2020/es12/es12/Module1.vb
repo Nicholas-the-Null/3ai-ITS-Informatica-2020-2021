45c851b787da9c6d5a9061cc0a96bfcf7a1f0462
Nicholas-scuola <56036434+nicholas-progetti-scuola@users.noreply.github.com>
2020-11-30 17:34:27 +0100

    Add files via upload

---
+++ informatica/17-11-2020/es12/es12/Module1.vb
@@ -0,0 +1,51 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim numero_a, numero_b, numero_c, appoggio As Integer
+        Dim sa, sb, sc As String
+        sa = ""
+        sb = ""
+        sc = ""
+        numero_a = InputBox("Inserire il numero di elementi del primo vettore", "RICHIESTA")
+        numero_b = InputBox("Inserire il numero di elementi del secondo vettore", "RICHIESTA")
+        Dim veta(numero_a), vetb(numero_b), vetc(0) As Integer
+
+        For x = 0 To numero_a + numero_b
+            If x < UBound(veta) Then
+                veta(x) = InputBox("Inserire l'elemento numero " & x & " del primo vettore", "RICHIESTA")
+                sa += veta(x).ToString + " "
+                vetc(numero_c) = veta(x)
+                numero_c += 1
+                ReDim Preserve vetc(numero_c)
+            End If
+
+            If x < UBound(vetb) Then
+                vetb(x) = InputBox("Inserire l'elemento numero " & x & " del secondo vettore", "RICHIESTA")
+                sb += vetb(x).ToString + " "
+                vetc(numero_c) = vetb(x)
+                numero_c += 1
+                ReDim Preserve vetc(numero_c)
+            End If
+
+
+        Next
+
+        For i = 0 To UBound(vetc) - 2
+            For j = i + 1 To UBound(vetc) - 1
+                If vetc(i) > vetc(j) Then
+                    appoggio = vetc(i)
+                    vetc(i) = vetc(j)
+                    vetc(j) = appoggio
+                End If
+            Next
+            sc += vetc(i).ToString + " "
+        Next
+
+        sc += vetc(UBound(vetc) - 1).ToString
+        MsgBox("Il primo vettore è : " & sa, MsgBoxStyle.Information, "INFORMAZIONE")
+        MsgBox("Il secondo vettore è : " & sb, MsgBoxStyle.Information, "INFORMAZIONE")
+        MsgBox("Il terzo vettore è : " & sc, MsgBoxStyle.Information, "INFORMAZIONE")
+
+    End Sub
+
+End Module
---
+++ informatica/17-11-2020/es18/es13/Module1.vb
@@ -0,0 +1,21 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim s As String = InputBox("Inserire una stringa", "RICHIESTA")
+        Dim save As String
+        Dim n, conta_numeri As Integer
+        n = InputBox("Inserire il valore ascii", "RICHIESTA")
+        For x = 0 To Len(s) - 1
+            If Asc(s(x)) > n Then
+                conta_numeri += 1
+                save += s(x) + " "
+            End If
+        Next
+        MsgBox("I caratteri che hanno un valore in codice ASCII maggiore di " & n & " sono : " & conta_numeri & ".", MsgBoxStyle.Information, "Informazione")
+        MsgBox("log lettere" & save)
+    End Sub
+
+
+
+
+End Module
