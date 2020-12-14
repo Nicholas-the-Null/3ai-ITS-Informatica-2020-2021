9854a1ad1b0f45f242a826c7b1c383643e790f1d
Nicholas-scuola <56036434+nicholas-progetti-scuola@users.noreply.github.com>
2020-12-14 18:18:02 +0100

    Add files via upload

---
+++ informatica/08-12-2020/esLibro8/esLibro8/Module1.vb
@@ -0,0 +1,35 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim n1, n2, appoggio As Integer
+        Dim s3 As String = ""
+
+        n1 = InputBox("Inserire la grandezza del primo vettore", "RICHIESTA")
+        n2 = InputBox("Inserire la grandezza del secondo vettore", "RICHIESTA")
+        If n1 < n2 Then
+            appoggio = n1
+            n1 = n2
+            n2 = appoggio
+        End If
+        Dim vet1(n1), vet2(n2), vet3(n1) As Integer
+        For x = 0 To UBound(vet1) - 1
+            vet1(x) = InputBox("Inserire un valore nel primo vettore", "RICHIESTA")
+            vet2(x) = InputBox("Inserire un elemento nel secondo vettore", "RICHIESTA")
+            If x > n2 Then
+                vet3(x) = vet1(x)
+                s3 += vet3(x) + " "
+            Else
+                If x Mod 2 = 0 Then
+                    vet3(x) = vet1(x)
+                    s3 += vet3(x) + " "
+                Else
+                    vet3(x) = vet2(x)
+                    s3 += vet3(x) + " "
+                End If
+            End If
+        Next
+
+        MsgBox("Il nuovo vettore è : " & s3, MsgBoxStyle.Information, "INFORMAZIONE")
+    End Sub
+
+End Module
---
+++ informatica/08-12-2020/esLibro8/esLibro8/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Le informazioni generali relative a un assembly sono controllate dal seguente 
+' set di attributi. Modificare i valori di questi attributi per modificare le informazioni
+' associate a un assembly.
+
+' Controllare i valori degli attributi degli assembly
+
+<Assembly: AssemblyTitle("esLibro8")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("esLibro8")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Se il progetto viene esposto a COM, il GUID seguente verrà usato come ID del typelib
+<Assembly: Guid("32026de8-8152-498b-9a42-29573e292aa1")>
+
+' Le informazioni sulla versione di un assembly sono costituite dai seguenti quattro valori:
+'
+'      Versione principale
+'      Versione secondaria
+'      Numero di build
+'      Revisione
+'
+' È possibile specificare tutti i valori oppure impostare valori predefiniti per i numeri relativi alla revisione e alla build
+' usando l'asterisco '*' come illustrato di seguito:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
